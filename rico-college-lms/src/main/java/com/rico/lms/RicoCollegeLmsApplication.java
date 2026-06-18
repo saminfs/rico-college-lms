b2b8ef1c2c8094fa615ab94c9a3a2c904408acf2
Eusha Ibne Nur Sami <saminfs9@gmail.com>
2026-06-18 06:41:44 +0100

    Initialize Rico College LMS Spring Boot project

---
+++ rico-college-lms/src/main/java/com/rico/lms/RicoCollegeLmsApplication.java
@@ -0,0 +1,13 @@
+package com.rico.lms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RicoCollegeLmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RicoCollegeLmsApplication.class, args);
+	}
+
+}
---
+++ rico-college-lms/src/test/java/com/rico/lms/RicoCollegeLmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rico.lms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RicoCollegeLmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
